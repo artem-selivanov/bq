7691e1cc87a1377ecb93d28b8f5f4313221ff806
Artem <webgid.main@gmail.com>
2025-03-24 18:58:54 +0200

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { BigQuery } = require('@google-cloud/bigquery');
 
 const app = express();
-const port = 3000;
+const port = 5100;
 
 // Используем переменные из .env
 const API_KEY = process.env.API_KEY;
